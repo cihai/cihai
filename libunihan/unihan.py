c6191d1298c9b5a25e731f71428b169297c827f7
Tony Narlock <tony@git-pull.com>
2013-12-06 15:33:00 +0800

    test reading csv file, retreiving datafiles

--- libunihan/unihan.py
+++ libunihan/unihan.py
@@ -11,6 +11,22 @@ libunihan.unihan
 
 from __future__ import absolute_import, division, print_function, with_statement
 
+from pkg_resources \
+    import resource_filename  # @UnresolvedImport #pylint: disable=E0611
+import zipfile
+import csv
+
+
+def get_datafile(file_):
+    return resource_filename(__name__, 'data/' + file_)
+
 
 def main():
-    print('hi')
+    print('%s' % get_datafile('Unihan.zip'))
+    z = zipfile.ZipFile(get_datafile('Unihan.zip'))
+    print([f.filename for f in z.filelist])
+
+    with open(get_datafile('Unihan_Readings.txt'), 'rb') as csvfile:
+        r = csv.reader(csvfile)
+        for row in r:
+            print(', '.join(row))
