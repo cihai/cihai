331661c1193b39f70798610ec4a6fe92b5183566
Tony Narlock <tony@git-pull.com>
2013-12-06 17:46:07 +0800

    Latest unihan.py

--- libunihan/unihan.py
+++ libunihan/unihan.py
@@ -21,8 +21,8 @@ def get_datafile(file_):
     return resource_filename(__name__, 'data/' + file_)
 
 
-def unichr3( *args ):
-    return [unichr( int( i[2:7], 16 ) ) for i in args if i[2:7]][0]
+def unichr3(*args):
+    return [unichr(int(i[2:7], 16)) for i in args if i[2:7]][0]
 
 
 def main():
@@ -31,19 +31,32 @@ def main():
     print([f.filename for f in z.filelist])
 
     with open(get_datafile('Unihan_Readings.txt'), 'rb') as csvfile:
-        csvfile = filter(lambda row: row[0]!='#', csvfile)
+        csvfile = filter(lambda row: row[0] != '#', csvfile)
         #r = csv.reader(csvfile)
         r = UnihanReader(
             csvfile,
             fieldnames=['char', 'field', 'value'],
             delimiter='\t'
         )
-        for row in r:
+        for row in list(r)[:5]:
             #print(', '.join(row))
             #print(unichr3(row['char']))
             print(row)
+            pass
 
 
 class UnihanReader(csv.DictReader):
     def __init__(self, *args, **kwargs):
         csv.DictReader.__init__(self, *args, **kwargs)
+
+    def next(self):
+        row = csv.DictReader.next(self)
+        for key in self.fieldnames:
+            if None:
+                row[key] = unichr3(row[key])
+
+        return row
+
+
+#  _neilg | borneo: you should be able to use regular expressions to convert
+#  U+([a-f0-9]+)\b to \U[a-f0-9]{8}
