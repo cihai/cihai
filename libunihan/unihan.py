cd8421a5100424e98d2d0cd30ef9f86fa8bba7f9
Tony Narlock <tony@git-pull.com>
2013-12-06 16:08:11 +0800

    latest

--- libunihan/unihan.py
+++ libunihan/unihan.py
@@ -21,12 +21,29 @@ def get_datafile(file_):
     return resource_filename(__name__, 'data/' + file_)
 
 
+def unichr3( *args ):
+    return [unichr( int( i[2:7], 16 ) ) for i in args if i[2:7]][0]
+
+
 def main():
     print('%s' % get_datafile('Unihan.zip'))
     z = zipfile.ZipFile(get_datafile('Unihan.zip'))
     print([f.filename for f in z.filelist])
 
     with open(get_datafile('Unihan_Readings.txt'), 'rb') as csvfile:
-        r = csv.reader(csvfile)
+        csvfile = filter(lambda row: row[0]!='#', csvfile)
+        #r = csv.reader(csvfile)
+        r = UnihanReader(
+            csvfile,
+            fieldnames=['char', 'field', 'value'],
+            delimiter='\t'
+        )
         for row in r:
-            print(', '.join(row))
+            #print(', '.join(row))
+            #print(unichr3(row['char']))
+            print(row)
+
+
+class UnihanReader(csv.DictReader):
+    def __init__(self, *args, **kwargs):
+        csv.DictReader.__init__(self, *args, **kwargs)
