621e78672b929ee95562c536f51cd1018cbabde8
Tony Narlock <tony@git-pull.com>
2013-12-19 09:05:21 +0800

    Example documentation.

--- cihai/testsuite/test_middleware/simple/__init__.py
+++ cihai/testsuite/test_middleware/simple/__init__.py
@@ -15,7 +15,26 @@ from __future__ import absolute_import, division, print_function, \
 
 class DatasetExample(object):
 
+    """Sample dataset to demonstrate a simple extension with Cihai.
+
+    This object is passed into :meth:`cihai.Cihai.use` in the
+    :class:`cihai.Cihai` object.
+
+    """
+
     def get(self, request, response):
+        """Return chinese character data from sample dataset.
+
+        The source of information in this example is a :obj:`dict`. In real
+        modules, the dataset may be a CSV, sqlite database, or an API. As long
+        as the data appends to the :param:`response` and returns it.
+
+        :param request: The character or string being looked up, e.g. '好'.
+        :type request: string
+        :returns: Cihai response dictionary
+        :rtype: dict
+
+        """
         dataset = {
             '好': {
                 'definition': 'hao'
@@ -28,6 +47,24 @@ class DatasetExample(object):
         return response
 
     def reverse(self, request, response):
+        """Return chinese character data from a reverse lookup sample dataset.
+
+        :param request: The character or string being looked up, e.g. '好'.
+        :type request: string
+        :returns: Cihai reverse look up. The results should be formatted as::
+
+            {
+                '好': {
+                    'definition': 'good.'
+                }
+            }
+
+        When the character already exists in :param:`response`, the character
+        ``好`` must be mixed-in with, not overwritten.
+
+        :rtype: dict
+
+        """
         dataset = {
             '好': {
                 'definition': 'hao'
