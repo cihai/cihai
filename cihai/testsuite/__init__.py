e78ee1b33e7b54f4e508fe953c70d567abc59432
Tony Narlock <tony@git-pull.com>
2014-01-16 12:27:45 +0800

    move werkzeug methods for module finding to utils.

--- cihai/testsuite/__init__.py
+++ cihai/testsuite/__init__.py
@@ -14,6 +14,7 @@ import sys
 
 from ..log import DebugLogFormatter
 from .._compat import string_types, PY2, reraise
+from ..util import find_modules
 
 import pkgutil
 
@@ -29,125 +30,6 @@ if not log.handlers:
     testsuite_logger = logging.getLogger(__name__)
 
     testsuite_logger.setLevel('INFO')
-
-
-class ImportStringError(ImportError):
-    """Provides information about a failed :func:`import_string` attempt."""
-
-    #: String in dotted notation that failed to be imported.
-    import_name = None
-    #: Wrapped exception.
-    exception = None
-
-    def __init__(self, import_name, exception):
-        self.import_name = import_name
-        self.exception = exception
-
-        msg = (
-            'import_string() failed for %r. Possible reasons are:\n\n'
-            '- missing __init__.py in a package;\n'
-            '- package or module path not included in sys.path;\n'
-            '- duplicated package or module name taking precedence in '
-            'sys.path;\n'
-            '- missing module, class, function or variable;\n\n'
-            'Debugged import:\n\n%s\n\n'
-            'Original exception:\n\n%s: %s')
-
-        name = ''
-        tracked = []
-        for part in import_name.replace(':', '.').split('.'):
-            name += (name and '.') + part
-            imported = import_string(name, silent=True)
-            if imported:
-                tracked.append((name, getattr(imported, '__file__', None)))
-            else:
-                track = ['- %r found in %r.' % (n, i) for n, i in tracked]
-                track.append('- %r not found.' % name)
-                msg = msg % (import_name, '\n'.join(track),
-                             exception.__class__.__name__, str(exception))
-                break
-
-        ImportError.__init__(self, msg)
-
-    def __repr__(self):
-        return '<%s(%r, %r)>' % (self.__class__.__name__, self.import_name,
-                                 self.exception)
-
-
-def import_string(import_name, silent=False):
-    """Imports an object based on a string.  This is useful if you want to
-    use import paths as endpoints or something similar.  An import path can
-    be specified either in dotted notation (``xml.sax.saxutils.escape``)
-    or with a colon as object delimiter (``xml.sax.saxutils:escape``).
-
-    If `silent` is True the return value will be `None` if the import fails.
-
-    :param import_name: the dotted name for the object to import.
-    :param silent: if set to `True` import errors are ignored and
-                   `None` is returned instead.
-    :return: imported object
-    """
-    #XXX: py3 review needed
-    assert isinstance(import_name, string_types)
-    # force the import name to automatically convert to strings
-    import_name = str(import_name)
-    try:
-        if ':' in import_name:
-            module, obj = import_name.split(':', 1)
-        elif '.' in import_name:
-            module, obj = import_name.rsplit('.', 1)
-        else:
-            return __import__(import_name)
-        # __import__ is not able to handle unicode strings in the fromlist
-        # if the module is a package
-        if PY2 and isinstance(obj, unicode):
-            obj = obj.encode('utf-8')
-        try:
-            return getattr(__import__(module, None, None, [obj]), obj)
-        except (ImportError, AttributeError):
-            # support importing modules not yet set up by the parent module
-            # (or package for that matter)
-            modname = module + '.' + obj
-            __import__(modname)
-            return sys.modules[modname]
-    except ImportError as e:
-        if not silent:
-            reraise(
-                ImportStringError,
-                ImportStringError(import_name, e),
-                sys.exc_info()[2])
-
-
-def find_modules(import_path, include_packages=False, recursive=False):
-    """Find all the modules below a package.  This can be useful to
-    automatically import all views / controllers so that their metaclasses /
-    function decorators have a chance to register themselves on the
-    application.
-
-    Packages are not returned unless `include_packages` is `True`.  This can
-    also recursively list modules but in that case it will import all the
-    packages to get the correct load path of that module.
-
-    :param import_name: the dotted name for the package to find child modules.
-    :param include_packages: set to `True` if packages should be returned, too.
-    :param recursive: set to `True` if recursion should happen.
-    :return: generator
-    """
-    module = import_string(import_path)
-    path = getattr(module, '__path__', None)
-    if path is None:
-        raise ValueError('%r is not a package' % import_path)
-    basename = module.__name__ + '.'
-    for importer, modname, ispkg in pkgutil.iter_modules(path):
-        modname = basename + modname
-        if ispkg:
-            if include_packages:
-                yield modname
-            if recursive:
-                for item in find_modules(modname, include_packages, True):
-                    yield item
-        else:
-            yield modname
 
 
 def iter_suites(package):
