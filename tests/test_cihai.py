af49b28d31e2ebba3040946d1e1de191a73d49f5
Tony Narlock <tony@git-pull.com>
2017-05-13 13:13:05 -0500

    clean up tests

--- tests/test_cihai.py
+++ tests/test_cihai.py
@@ -9,19 +9,16 @@ using the ``test_config.yml``.
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
-import os
-
-import cihai
 from cihai.core import Cihai
 
 
 def test_config_defaults():
     """Test config defaults."""
 
-    cihai = Cihai.from_file()
+    app = Cihai.from_file()
 
-    assert 'debug' in cihai.config
-    assert not cihai.config['debug']
+    assert 'debug' in app.config
+    assert not app.config['debug']
 
 
 def test_config_dict_args():
@@ -29,29 +26,16 @@ def test_config_dict_args():
 
     expected = 'world'
 
-    cihai = Cihai({
+    app = Cihai({
         'hello': expected
     })
 
-    result = cihai.config['hello']
+    result = app.config['hello']
 
     assert result == expected
 
 
 def test_yaml_config_and_override(test_config_file):
-    cihai = Cihai.from_cli(['-c', test_config_file])
+    app = Cihai.from_cli(['-c', test_config_file])
 
-    assert cihai.config['debug']
-
-
-def test_data_path_default():
-
-    expected = os.path.abspath(os.path.join(
-        os.path.dirname(cihai.__file__),
-        'data/'
-    ))
-
-    c = Cihai.from_file()
-    result = c.config['data_path']
-
-    assert expected == result
+    assert app.config['debug']
