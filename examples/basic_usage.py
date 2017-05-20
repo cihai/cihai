7727c310264361caae62cd5357ecb6d7a57c4db0
Tony Narlock <tony@git-pull.com>
2017-05-20 06:20:27 -0500

    add basic python example

---
+++ examples/basic_usage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf8 - *-
+
+from __future__ import unicode_literals, print_function
+
+from cihai.core import Cihai
+from cihai.bootstrap import bootstrap_unihan
+
+c = Cihai()
+if not c.is_bootstrapped:  # download and install Unihan to db
+    bootstrap_unihan(c.metadata)
+    c.reflect_db()         # automap new table created during bootstrap
+
+query = c.lookup_char('好')
+glyph = query.first()
+print("lookup for 好: %s" % glyph.kDefinition)
+
+query = c.reverse_char('good')
+print('matches for "good": %s ' % ', '.join([glyph_.char for glyph_ in query]))
