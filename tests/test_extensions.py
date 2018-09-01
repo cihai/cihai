7bcac162eb64269a9b6b7a4daef587d971896cb1
Tony Narlock <tony@git-pull.com>
2018-09-01 15:11:11 -0500

    stub test for adding an extension, simple lookup

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -2,6 +2,18 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from cihai import extension
+from cihai.core import Cihai
 
-def test_add_extensions():
-    assert extension.Extension
+
+class SimplestDataset(extension.Dataset):
+    def lookup(self, search):
+        return 'hi'
+
+def test_add_extensions2():
+    c = Cihai()
+    c.add_dataset(SimplestDataset, namespace='simple')
+    assert hasattr(c, 'simple')
+    assert isinstance(c.simple, extension.Dataset)
+    assert hasattr(c.simple, 'simple', 'lookup')
+    assert callable(c.simple.lookup)
+    assert c.simple.lookup() == 'hi'
