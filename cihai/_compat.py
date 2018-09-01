81e3ae96b4c33b6d6f8dba1e4e3a6867cefba3c8
Tony Narlock <tony@git-pull.com>
2018-09-01 13:28:54 -0500

    Port with_metaclass from flask
    For python 2/3 compatibility
    
    License BSD
    
    See also:
    - https://github.com/pallets/flask/blob/1.0.2/LICENSE
    - https://github.com/pallets/flask/blob/1.0.2/flask/_compat.py
    - Fef: 310fbfc on Feb 8th, 2018

--- cihai/_compat.py
+++ cihai/_compat.py
@@ -47,3 +47,14 @@ else:
         if value.__traceback__ is not tb:
             raise (value.with_traceback(tb))
         raise value
+
+
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a
+    # dummy metaclass for one level of class instantiation that replaces
+    # itself with the actual metaclass.
+    class metaclass(type):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, 'temporary_class', (), {})
