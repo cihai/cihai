bf5b39a84cf3415aa0cbaa11d1c808035d1404d4
Tony Narlock <tony@git-pull.com>
2018-09-01 13:28:44 -0500

    Boiler plate extension test

---
+++ tests/test_extensions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8 - *-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cihai import extension
+
+def test_add_extensions():
+    assert extension.Extension
