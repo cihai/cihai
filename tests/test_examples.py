a1eb0ac84cb7e5217d1e25359f2f292b6098724a
Tony Narlock <tony@git-pull.com>
2018-09-02 13:14:17 -0500

    Test examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,18 @@
 # -*- coding: utf8 - *-
 from __future__ import absolute_import, print_function, unicode_literals
 
+import examples.basic_usage
+import examples.variant_ts_difficulties
 import examples.variants
 
 
-def test_examples(unihan_options):
-    examples.variants.script(unihan_options=unihan_options)
+def test_variants(unihan_options):
+    examples.variants.run(unihan_options=unihan_options)
+
+
+def test_ts_difficulties(unihan_options):
+    examples.variant_ts_difficulties.run(unihan_options=unihan_options)
+
+
+def test_basic_usage(unihan_options):
+    examples.basic_usage.run(unihan_options=unihan_options)
