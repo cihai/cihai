84da43bef7e2a0ca412243d8e7885a8ff5a9e3bc
Tony Narlock <tony@git-pull.com>
2018-09-02 10:26:43 -0500

    Add import_string from werkzeug

--- cihai/utils.py
+++ cihai/utils.py
@@ -7,6 +7,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 import collections
 import sys
 
+from . import exc
+from ._compat import reraise
+
 
 def merge_dict(base, additional):
     """
@@ -60,3 +63,72 @@ def supports_wide():
         True if python supports wide character sets
     """
     return sys.maxunicode > 0xffff
+
+
+def import_string(import_name, silent=False):  # NOQA: C901
+    """
+    Imports an object based on a string.
+
+    This is useful if you want to use import paths as endpoints or
+    something similar.  An import path can  be specified either in dotted
+    notation (``xml.sax.saxutils.escape``) or with a colon as object
+    delimiter (``xml.sax.saxutils:escape``).
+
+    If `silent` is True the return value will be `None` if the import fails.
+
+    Parameters
+    ----------
+    import_name : string
+        the dotted name for the object to import.
+    silent : bool
+        if set to `True` import errors are ignored and `None` is returned instead.
+
+    Returns
+    -------
+    imported object
+
+    Raises
+    ------
+    cihai.exc.ImportStringError (ImportError, cihai.exc.CihaiException)
+
+    Notes
+    -----
+    This is from werkzeug.utils c769200 on May 23, LICENSE BSD.
+    https://github.com/pallets/werkzeug
+
+    Changes:
+    - Exception raised is cihai.exc.ImportStringError
+    - Add NOQA C901 to avoid complexity lint
+    """
+    # force the import name to automatically convert to strings
+    # __import__ is not able to handle unicode strings in the fromlist
+    # if the module is a package
+    import_name = str(import_name).replace(':', '.')
+    try:
+        try:
+            __import__(import_name)
+        except ImportError:
+            if '.' not in import_name:
+                raise
+        else:
+            return sys.modules[import_name]
+
+        module_name, obj_name = import_name.rsplit('.', 1)
+        try:
+            module = __import__(module_name, None, None, [obj_name])
+        except ImportError:
+            # support importing modules not yet set up by the parent module
+            # (or package for that matter)
+            module = import_string(module_name)
+
+        try:
+            return getattr(module, obj_name)
+        except AttributeError as e:
+            raise ImportError(e)
+
+    except ImportError as e:
+        if not silent:
+            reraise(
+                exc.ImportStringError,
+                exc.ImportStringError(import_name, e),
+                sys.exc_info()[2])
