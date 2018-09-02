277a16154538be0041b7316731888b262c3ceb05
Tony Narlock <tony@git-pull.com>
2018-09-02 13:35:35 -0500

    Add dataset example

---
+++ examples/dataset.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 - *-
+from __future__ import print_function, unicode_literals
+
+from cihai.core import Cihai
+from cihai.extension import Dataset
+
+data = {}  # any data source, internal, a file, on the internet, in a database...
+
+class MyDataset(Dataset):
+    def bootstrap(self):  # automatically ran with .add_dataset, if exists
+        # Use this to setup your dataset, check if updates are needed, etc.
+        data.update({
+            '好': 'Good',
+            '好好': 'Hello',
+        })
+
+    def givemedata(self, key):
+        return data[key]
+
+    def search(self, needle):
+        return {k: v for k, v in data.items() if needle in k}
+
+    def backwards(self, needle):
+        return [k for k, v in data.items() if needle in v]
+
+
+def run():
+    c = Cihai()
+
+    c.add_dataset(MyDataset, namespace='moo')
+
+    print('Definitions exactly for 好', c.moo.givemedata('好'))
+
+    print('Definitions matching with 你好:', ', '.join(c.moo.search('好')))
+
+    print('Reverse definition with Good:', ', '.join(c.moo.backwards('Good')))
+
+
+if __name__ == '__main__':
+    run()
