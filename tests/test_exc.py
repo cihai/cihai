17d91eff7de5517aa89330a08f3c84fa46d02538
Tony Narlock <tony@git-pull.com>
2019-08-17 09:45:24 -0500

    Update exception test for pytest 5+
    pytest 3 had message for raises, this is removed in current versions.

--- tests/test_exc.py
+++ tests/test_exc.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-
 import pytest
 
 from cihai import exc
 
 
 def test_base_exception():
-    with pytest.raises(
-        exc.CihaiException,
-        message="Make sure no one removes or renames base CihaiException",
-    ):
-        raise exc.CihaiException()
+    with pytest.raises(exc.CihaiException):
+        raise exc.CihaiException()  # Make sure its base of CihaiException
 
-    with pytest.raises(Exception, message="Extends python base exception"):
-        raise exc.CihaiException()
+    with pytest.raises(Exception):
+        raise exc.CihaiException()  # Extends python base exception
