bfa765222dbe3a96f2562d9ceb088ddb8aaa1c37
Tony Narlock <tony@git-pull.com>
2023-12-09 10:45:19 -0600

    utils: pydocstyle manual fixes

--- src/cihai/utils.py
+++ src/cihai/utils.py
@@ -17,7 +17,7 @@ def supports_wide() -> bool:
 
 
 def import_string(import_name: str, silent: bool = False) -> t.Any:
-    """Imports an object based on a string.
+    """Import an object based on a string.
 
     This is useful if you want to use import paths as endpoints or
     something similar.  An import path can  be specified either in dotted
