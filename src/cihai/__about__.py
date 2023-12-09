bc4190ec6bf3368a22c2865743419f65bd19b6a9
Tony Narlock <tony@git-pull.com>
2023-12-09 13:53:06 -0600

    Tag v0.30.0 (pydocstyle docstrings, bug fixes)

--- src/cihai/__about__.py
+++ src/cihai/__about__.py
@@ -1,7 +1,7 @@
 """Cihai Metadata package."""
 __title__ = "cihai"
 __package_name__ = "cihai"
-__version__ = "0.29.0"
+__version__ = "0.30.0"
 __description__ = "Library for CJK (chinese, japanese, korean) language data."
 __author__ = "Tony Narlock"
 __email__ = "tony@git-pull.com"
