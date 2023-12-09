e3779f3a65e5132ea553ffe9a6af5ebd5361947a
Tony Narlock <tony@git-pull.com>
2023-12-09 09:44:24 -0600

    tests(exc): pydocstyle manual fixes

--- tests/test_exc.py
+++ tests/test_exc.py
@@ -1,9 +1,11 @@
+"""Exception tests for cihai."""
 import pytest
 
 from cihai import exc
 
 
 def test_base_exception() -> None:
+    """Test basic Cihai exceptions."""
     with pytest.raises(exc.CihaiException):
         raise exc.CihaiException()  # Make sure its base of CihaiException
 
