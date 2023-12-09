1101cb3351f9c71d9919576b2d52f220510544ae
Tony Narlock <tony@git-pull.com>
2023-12-09 10:57:32 -0600

    example(dataset): pydocstyle manual fixes

--- examples/dataset.py
+++ examples/dataset.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Example of a custom dataset for cihai."""
 import typing as t
 
 from cihai.core import Cihai
@@ -8,21 +9,28 @@ data = {}  # any data source, internal, a file, on the internet, in a database..
 
 
 class MyDataset(Dataset):
+    """Hardcoded example dataset for Cihai."""
+
     def bootstrap(self) -> None:  # automatically ran with .add_dataset, if exists
+        """Initialize hard-coded dataset."""
         # Use this to setup your dataset, check if updates are needed, etc.
         data.update({"好": "Good", "好好": "Hello"})
 
     def givemedata(self, key: str) -> str:
+        """Return data via direct key match."""
         return data[key]
 
     def search(self, needle: str) -> t.Dict[str, object]:
+        """Return key-value mapping of keys matching a subset of value."""
         return {k: v for k, v in data.items() if needle in k}
 
     def backwards(self, needle: str) -> t.List[str]:
+        """Reverse lookup."""
         return [k for k, v in data.items() if needle in v]
 
 
 def run() -> None:
+    """Run hard-coded example dataset."""
     c = Cihai(unihan=False)
 
     c.add_dataset(MyDataset, namespace="moo")
